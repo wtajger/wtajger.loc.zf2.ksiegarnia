cf15e5d4e74762979318b7a4f35ed16e179f8276
a <a>
2015-08-28 10:13:56 +0200

    bledna lokalizacja Ksigarni w Application/src

--- module/Application/src/Ksiegarnia/config/module.config.php
+++ module/Application/src/Ksiegarnia/config/module.config.php
@@ -15,7 +15,7 @@ return array(
                 'options' => array(
                     'route'    => '/',
                     'defaults' => array(
-                        'controller' => 'Application\Controller\Index',
+                        'controller' => 'Ksiegarnia\Controller\Index',
                         'action'     => 'index',
                     ),
                 ),
@@ -24,12 +24,12 @@ return array(
             // new controllers and actions without needing to create a new
             // module. Simply drop new controllers in, and you can access them
             // using the path /application/:controller/:action
-            'application' => array(
+            'ksiegarnia' => array(
                 'type'    => 'Literal',
                 'options' => array(
-                    'route'    => '/application',
+                    'route'    => '/ksiegarnia',
                     'defaults' => array(
-                        '__NAMESPACE__' => 'Application\Controller',
+                        '__NAMESPACE__' => 'Ksiegarnia\Controller',
                         'controller'    => 'Index',
                         'action'        => 'index',
                     ),
@@ -73,7 +73,7 @@ return array(
     ),
     'controllers' => array(
         'invokables' => array(
-            'Application\Controller\Index' => 'Application\Controller\IndexController'
+            'Ksiegarnia\Controller\Index' => 'Ksiegarnia\Controller\IndexController'
         ),
     ),
     'view_manager' => array(
@@ -84,7 +84,7 @@ return array(
         'exception_template'       => 'error/index',
         'template_map' => array(
             'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
-            'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
+            'ksiegarnia/index/index' => __DIR__ . '/../view/ksiegarnia/index/index.phtml',
             'error/404'               => __DIR__ . '/../view/error/404.phtml',
             'error/index'             => __DIR__ . '/../view/error/index.phtml',
         ),
